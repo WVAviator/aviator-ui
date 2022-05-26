9a3b66d1b75687669de8265fd44df0e0ddc29f2b
WVAviator <wvaviator@gmail.com>
2022-05-25 19:12:21 -0500

    added some documentation

--- src/theme/ThemeProvider.tsx
+++ src/theme/ThemeProvider.tsx
@@ -1,9 +1,20 @@
 import {
+	Theme,
 	ThemeProvider as EmotionThemeProvider,
-	ThemeProviderProps,
+	ThemeProviderProps as EmotionThemeProviderProps,
 } from "@emotion/react";
 import React from "react";
 
+interface ThemeProviderProps extends EmotionThemeProviderProps {
+	/**
+	 * The theme that will be applied to any AviatorUI components rendered in this context.
+	 */
+	theme: Theme;
+}
+
+/**
+ * Wrap your application in this ThemeProvider component to change the theme of all nested AviatorUI components. Use the createTheme function to create a new theme and assign it to the theme prop.
+ */
 const ThemeProvider: React.FC<ThemeProviderProps> = ({ children, theme }) => {
 	return (
 		<EmotionThemeProvider theme={theme}>{children}</EmotionThemeProvider>
